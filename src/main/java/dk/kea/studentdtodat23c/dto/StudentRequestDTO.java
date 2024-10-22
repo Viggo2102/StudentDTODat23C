b8fd7131654aa19db93112b2efec04de39de9e1d
Viggo2102 <146332112+Viggo2102@users.noreply.github.com>
2024-10-22 14:47:37 +0200

    Added DTO

---
+++ src/main/java/dk/kea/studentdtodat23c/dto/StudentRequestDTO.java
@@ -0,0 +1,8 @@
+package dk.kea.studentdtodat23c.dto;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public record StudentRequestDTO(String name, String password, LocalDate birthDate, LocalTime bornTime) {
+
+}
--- src/main/java/dk/kea/studentdtodat23c/model/Student.java
+++ src/main/java/dk/kea/studentdtodat23c/model/Student.java
@@ -4,11 +4,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
 
     @Id
@@ -27,57 +35,6 @@ public class Student {
         this.name = name;
         this.password = password;
         this.bornDate = bornDate;
-        this.bornTime = bornTime;
-    }
-
-    public Student(Long id, String name, String password, LocalDate bornDate, LocalTime bornTime) {
-        this.id = id;
-        this.name = name;
-        this.password = password;
-        this.bornDate = bornDate;
-        this.bornTime = bornTime;
-    }
-
-    public Student() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public LocalDate getBornDate() {
-        return bornDate;
-    }
-
-    public void setBornDate(LocalDate bornDate) {
-        this.bornDate = bornDate;
-    }
-
-    public LocalTime getBornTime() {
-        return bornTime;
-    }
-
-    public void setBornTime(LocalTime bornTime) {
         this.bornTime = bornTime;
     }
 }
